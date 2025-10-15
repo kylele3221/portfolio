adaf491fa7119852944aedfacc92b44b67a894ee
kylele3221 <kylele3221@gmail.com>
2025-10-15 14:33:47 -0700

    Finished Step 4

--- global.js
+++ global.js
@@ -44,3 +44,29 @@ for (let p of pages) {
 
   nav.append(a);
 }
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+  <label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+  `
+);
+
+const select = document.querySelector('.color-scheme select');
+
+if ("colorScheme" in localStorage) {
+  select.value = localStorage.colorScheme;
+  document.documentElement.style.setProperty('color-scheme', select.value);
+}
+
+select.addEventListener('input', function (event) {
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+  localStorage.colorScheme = event.target.value;
+});
