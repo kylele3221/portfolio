e6c81b64478902cd9c05e50d88ed33906d2324a6
Kyle Le <113484711+kylele3221@users.noreply.github.com>
2025-10-24 00:54:16 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,1 +1,5 @@
+import { fetchJSON } from '../global.js';
 
+const data = await fetchJSON('../lib/projects.json');
+const container = document.querySelector('.projects');
+console.log('projects loaded:', data, container);
