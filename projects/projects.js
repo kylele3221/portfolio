4365effd7708565727c53979ca45405b0aaf825d
Kyle Le <113484711+kylele3221@users.noreply.github.com>
2025-10-24 13:53:59 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -3,3 +3,6 @@ import { fetchJSON, renderProjects } from '../global.js';
 const data = await fetchJSON('../lib/projects.json');
 const container = document.querySelector('.projects');
 renderProjects(data, container, 'h2');
+
+const title = document.querySelector('.projects-title');
+if (title) title.textContent = `Projects (${data.length})`;
