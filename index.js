66537a65bffe2e364c974356d5b812f178e3c5c8
Kyle Le <113484711+kylele3221@users.noreply.github.com>
2025-10-24 13:59:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ const latest = all.slice(-3);
 
 const homeContainer = document.querySelector('.projects');
 if (homeContainer) renderProjects(latest, homeContainer, 'h3');
+
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
+
+const all = await fetchJSON('./lib/projects.json');
+const latest = all.slice(-3);
+const homeContainer = document.querySelector('.projects');
+if (homeContainer) renderProjects(latest, homeContainer, 'h3');
+
+const profileStats = document.querySelector('#profile-stats');
+if (profileStats) {
+  const d = await fetchGitHubData('kylele3221');
+  profileStats.innerHTML = `
+    <dl>
+      <dt>Public Repos:</dt><dd>${d.public_repos}</dd>
+      <dt>Public Gists:</dt><dd>${d.public_gists}</dd>
+      <dt>Followers:</dt><dd>${d.followers}</dd>
+      <dt>Following:</dt><dd>${d.following}</dd>
+    </dl>
+  `;
+}
