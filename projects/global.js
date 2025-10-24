a6b1d07ba6e3ec73b97a64c918ac97dc759027b3
Kyle Le <113484711+kylele3221@users.noreply.github.com>
2025-10-23 23:48:22 -0700

    Create global.js

---
+++ projects/global.js
@@ -0,0 +1,12 @@
+export async function fetchJSON(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
