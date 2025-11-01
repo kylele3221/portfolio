f5343f714cbe1efc1c94f488a3aaa3da5816fdf6
Kyle Le <113484711+kylele3221@users.noreply.github.com>
2025-10-31 18:47:16 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,6 +1,8 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 import { fetchJSON, renderProjects } from '../global.js';
 
+let query = '';
+
 const projects = await fetchJSON('../lib/projects.json');
 const container = document.querySelector('.projects');
 renderProjects(projects, container, 'h2');
