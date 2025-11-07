b99bf8ac6b870004b0b26cd094430229db9ea5b7
Kyle Le <113484711+kylele3221@users.noreply.github.com>
2025-11-07 02:00:13 -0800

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -156,6 +156,12 @@ function renderScatterPlot(data, commits) {
     .attr('transform', `translate(${usableArea.left}, 0)`)
     .call(yAxis);
 
+  const [minLines, maxLines] = d3.extent(commits, d => d.totalLines);
+  const rScale = d3
+    .scaleLinear()
+    .domain([minLines, maxLines])
+    .range([2, 30]);
+
   const dots = svg.append('g').attr('class', 'dots');
 
   dots
@@ -164,14 +170,17 @@ function renderScatterPlot(data, commits) {
     .join('circle')
     .attr('cx', d => xScale(d.datetime))
     .attr('cy', d => yScale(d.hourFrac))
-    .attr('r', 5)
+    .attr('r', d => rScale(d.totalLines))
     .attr('fill', 'steelblue')
+    .style('fill-opacity', 0.7)
     .on('mouseenter', (event, commit) => {
+      d3.select(event.currentTarget).style('fill-opacity', 1);
       renderTooltipContent(commit);
       updateTooltipVisibility(true);
       updateTooltipPosition(event);
     })
-    .on('mouseleave', () => {
+    .on('mouseleave', event => {
+      d3.select(event.currentTarget).style('fill-opacity', 0.7);
       updateTooltipVisibility(false);
     });
 }
