a5c80afd46515abbf557688f41e961f6be9d6bd1
Kyle Le <113484711+kylele3221@users.noreply.github.com>
2025-11-07 00:36:28 -0800

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -68,29 +68,25 @@ function renderCommitInfo(data, commits) {
   dl.append('dd').text(avgFileLength.toFixed(1));
 }
 
-let data = await loadData();
-let commits = processCommits(data);
-renderCommitInfo(data, commits);
-
 function renderScatterPlot(data, commits) {
-  const width = 1000
-  const height = 600
+  const width = 1000;
+  const height = 600;
 
   const svg = d3
     .select('#chart')
     .append('svg')
     .attr('viewBox', `0 0 ${width} ${height}`)
-    .style('overflow', 'visible')
+    .style('overflow', 'visible');
 
   const xScale = d3
     .scaleTime()
     .domain(d3.extent(commits, d => d.datetime))
     .range([0, width])
-    .nice()
+    .nice();
 
-  const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0])
+  const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
 
-  const dots = svg.append('g').attr('class', 'dots')
+  const dots = svg.append('g').attr('class', 'dots');
 
   dots
     .selectAll('circle')
@@ -99,12 +95,11 @@ function renderScatterPlot(data, commits) {
     .attr('cx', d => xScale(d.datetime))
     .attr('cy', d => yScale(d.hourFrac))
     .attr('r', 5)
-    .attr('fill', 'steelblue')
+    .attr('fill', 'steelblue');
 }
 
-let data = await loadData()
-let commits = processCommits(data)
+let data = await loadData();
+let commits = processCommits(data);
 
-renderCommitInfo(data, commits)
-renderScatterPlot(data, commits)
-
+renderCommitInfo(data, commits);
+renderScatterPlot(data, commits);
